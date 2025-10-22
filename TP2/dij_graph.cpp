7c7107324597af5f98a12e411eb7495d563e02eb
YosraGuesmi <guesmiyosra0@gmail.com>
2025-10-22 17:19:46 +0200

    solution exercice 5 question 4

--- TP2/dij_graph.cpp
+++ TP2/dij_graph.cpp
@@ -51,6 +51,174 @@ void question_5_1(ifstream &fin, ofstream &fout) {
 }
 
 
+
+void question_5_2(ifstream &fin, ofstream &fout) {
+    fout << "=== Question 5.2 : Dijkstra avec liste d'adjacence ===\n";
+
+    int n, m, start, dest;
+    fin >> n >> m >> start >> dest;
+
+    vector<vector<pair<int,int>>> adj(n + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w; fin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<int> dist(n + 1, INF), parent(n + 1, -1), vis(n + 1, 0);
+    dist[start] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        int u = -1;
+        for (int j = 1; j <= n; ++j)
+            if (!vis[j] && (u == -1 || dist[j] < dist[u])) u = j;
+        if (u == -1 || dist[u] == INF) break;
+        vis[u] = 1;
+
+        for (auto [v, w] : adj[u])
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+            }
+    }
+
+    if (dist[dest] == INF) {
+        fout << "Aucun chemin entre " << start << " et " << dest << "\n";
+        return;
+    }
+
+    fout << "Distance minimale = " << dist[dest] << "\n";
+    vector<int> path;
+    for (int v = dest; v != -1; v = parent[v]) path.push_back(v);
+    reverse(path.begin(), path.end());
+    fout << "Chemin le plus court : ";
+    for (size_t i = 0; i < path.size(); ++i) {
+        if (i) fout << " -> ";
+        fout << path[i];
+    }
+    fout << "\n";
+}
+
+
+
+void question_5_3(ifstream &fin, ofstream &fout) {
+    fout << "=== Question 5.3 : Dijkstra avec tas (min-heap) ===\n";
+
+    int n, m, start, dest;
+    fin >> n >> m >> start >> dest;
+
+    vector<vector<pair<int,int>>> adj(n + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w; fin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<int> dist(n + 1, INF), parent(n + 1, -1);
+    dist[start] = 0;
+
+    using pii = pair<int,int>;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > dist[u]) continue;
+        for (auto [v, w] : adj[u]) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    if (dist[dest] == INF) {
+        fout << "Aucun chemin entre " << start << " et " << dest << "\n";
+        return;
+    }
+
+    fout << dist[dest] << "\n";
+    vector<int> path;
+    for (int v = dest; v != -1; v = parent[v]) path.push_back(v);
+    reverse(path.begin(), path.end());
+    for (size_t i = 0; i < path.size(); ++i) {
+        if (i) fout << " -> ";
+        fout << path[i];
+    }
+    fout << "\n";
+}
+
+void question_5_4(ifstream &fin, ofstream &fout) {
+    fout << "=== Question 5.4 : Chemins les plus courts depuis une source ===\n";
+
+    int n, m, start, dest_unused;
+    fin >> n >> m >> start >> dest_unused;
+
+    if (start < 1 || start > n) {
+        fout << "Erreur : sommet de départ hors limites\n";
+        return;
+    }
+
+    const int INF = 1e9;
+
+    vector<vector<int>> adj(n + 1, vector<int>(n + 1, INF));
+    for (int i = 1; i <= n; ++i) adj[i][i] = 0;
+
+
+    for (int i = 0; i < m; ++i) {
+        int u, v, w;
+        fin >> u >> v >> w;
+        adj[u][v] = w;
+        adj[v][u] = w;
+    }
+
+
+    vector<int> dist(n + 1, INF), parent(n + 1, -1);
+    vector<bool> vis(n + 1, false);
+    dist[start] = 0;
+
+
+    for (int i = 1; i <= n; ++i) {
+        int u = -1;
+        for (int j = 1; j <= n; ++j)
+            if (!vis[j] && (u == -1 || dist[j] < dist[u])) u = j;
+        if (u == -1 || dist[u] == INF) break;
+        vis[u] = true;
+
+        for (int v = 1; v <= n; ++v)
+            if (adj[u][v] < INF && dist[v] > dist[u] + adj[u][v]) {
+                dist[v] = dist[u] + adj[u][v];
+                parent[v] = u;
+            }
+    }
+
+
+    for (int u = 1; u <= n; ++u) {
+        if (u == start) continue;
+        if (dist[u] == INF) {
+            fout << "Aucun chemin vers " << u << "\n";
+            continue;
+        }
+
+        fout << "Sommet " << start << " -> " << u
+             << " : distance = " << dist[u] << "\n";
+
+        vector<int> path;
+        for (int v = u; v != -1; v = parent[v]) path.push_back(v);
+        reverse(path.begin(), path.end());
+
+        fout << "Chemin : ";
+        for (size_t i = 0; i < path.size(); ++i) {
+            if (i) fout << " -> ";
+            fout << path[i];
+        }
+        fout << "\n";
+    }
+}
+
+
+
 int main() {
     ifstream fin("INPDIJGRAPH.TXT");
     ofstream fout("OUTDIJGRAPH.TXT");
@@ -61,7 +229,7 @@ int main() {
     }
 
 
-    question_5_1(fin, fout);
+    question_5_4(fin, fout);
 
     return 0;
 }
