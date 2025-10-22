888b286c90fb9204c89c765a2e3b9b2045478f8e
YosraGuesmi <guesmiyosra0@gmail.com>
2025-10-22 16:10:40 +0200

    solution exercice 5 question 2

--- TP2/dij_graph.cpp
+++ TP2/dij_graph.cpp
@@ -51,6 +51,54 @@ void question_5_1(ifstream &fin, ofstream &fout) {
 }
 
 
+void question_5_2(ifstream &fin, ofstream &fout) {
+    fout << "=== Question 5.2 : Dijkstra avec liste d'adjacence ===\n";
+
+    int n, m, start, dest;
+    fin >> n >> m >> start >> dest;
+
+    vector<vector<pair<int,int>>> adj(n + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w; fin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<int> dist(n + 1, INF), parent(n + 1, -1), vis(n + 1, 0);
+    dist[start] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        int u = -1;
+        for (int j = 1; j <= n; ++j)
+            if (!vis[j] && (u == -1 || dist[j] < dist[u])) u = j;
+        if (u == -1 || dist[u] == INF) break;
+        vis[u] = 1;
+
+        for (auto [v, w] : adj[u])
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+            }
+    }
+
+    if (dist[dest] == INF) {
+        fout << "Aucun chemin entre " << start << " et " << dest << "\n";
+        return;
+    }
+
+    fout << "Distance minimale = " << dist[dest] << "\n";
+    vector<int> path;
+    for (int v = dest; v != -1; v = parent[v]) path.push_back(v);
+    reverse(path.begin(), path.end());
+    fout << "Chemin le plus court : ";
+    for (size_t i = 0; i < path.size(); ++i) {
+        if (i) fout << " -> ";
+        fout << path[i];
+    }
+    fout << "\n";
+}
+
+
 
 int main() {
     ifstream fin("INPDIJGRAPH.TXT");
@@ -62,7 +110,7 @@ int main() {
     }
 
 
-    question_5_1(fin, fout);
+    question_5_2(fin, fout);
 
     return 0;
 }
